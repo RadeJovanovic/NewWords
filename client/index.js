e75f41aa970feff752f9051e217ced7ad4156dd1
Rad <rade.jovanovic360@gmail.com>
2016-03-30 15:21:51 +1100

    Fucking shit dunno bro

--- client/index.js
+++ client/index.js
@@ -1,4 +1,29 @@
 var myApp = angular.module('myApp',[])
-myApp.controller('MyController', function($scope){ //controller is basically in charge of taking care of the state of the page. Any variables in the controller, eg newWord, this is being controlled by MyController. the $scope means what the html file can access
+
+myApp.service('HistoryService', function($http){ //http because we are interfacing with the internet, whilst $scope interfaces with the html file
+        var baseUrl = "http://localhost:8080/" //This is just the address of server
+        this.saveWord = function(newWord){ //We will be sending this to the server
+            var url = baseUrl+"saveCurrent" //This is the address where we will send the stuff to
+            return $http.post(url,{"word":newWord}) //word:newWord is the data that we are actually sending
+            //Saveword is a function that is part of the myApp service, we use this service but putting the in value of the word from the html file
+        }
+        
+    }) //This service is a little module that handles interfacing with the server, whilst the controller is interfacing with the app
+
+
+
+myApp.controller('MyController', function($scope,HistoryService){ //controller is basically in charge of taking care of the state of the page. Any variables in the controller, eg newWord, this is being controlled by MyController. the $scope means what the html file can access, and HistoryService must be included because we are using that service rec 2:45
+    
+    
     $scope.newWord='cat' //js resets the box to cat every time you refresh the page
+    $scope.saveThisWord = function(){ //$scope means that the word will be accessible by the html file
+        HistoryService.saveWord($scope.newWord) //This line creates a promise that it will once come back with some value
+        .then(saveSuccess,error) //Once the value arrives, only then do we run the line - either saves successfully, or error if the server couldn't do what it was requested
+    }
+    function saveSuccess(json){ //do not need to be visible from the html page, so will not write $scope
+        console.log(json)   //The json 
+    }
+    function error(json){
+        console.log(err)
+    }
 });
--- server/server.js
+++ server/server.js
@@ -1,5 +1,24 @@
 var express = require('express') //refer to powerpoint
 var app = express() //Refer to powerpoint
+var bodyParser = require('body-parser') //checks for data that comes through with any request
+var Nedb = require('nedb')
+var database = new Nedb({})//creating a new database that we will be putting all or our data into
+
 app.use(express.static('../client')) //app.use() sits on the server and wait for requests from the internet
 //When those requests come in, they go through a pipeline of functions. So here, the first thing a request is going to do is that it is going to be checked as to whether it is in the root directory, which is the client folder (.. is back a folder from the server, then we go into the client folder)
+
+app.use(bodyParser.json()) //App.use is there because every request goes through it before being served rec 2:37
+app.post('/saveCurrent', function(req,res){ //if somebody posts something to "saveCurrent", it will console log whenever we send something to this address
+//    req.data; //Could record what we are getting in and what we are sending out
+//    res.send({})//Also, in this function, we have a whole bunch of information as well as the data
+    console.log("got a request to save something") 
+    //Now we will add the actual funcitonality
+    var data = {word:req.body.word, //We are sending up to the server is a json packet request, and we are trying to access the word property of it
+                date:Date.now()} //we are creating a json object called data, with two porperties
+    var done = function(){
+        console.log("I just wrote something to the database!!!")
+        res.end("done") //we have finished processing the request
+    }
+    database.insert(data,done) //insert the data and then run the done function THIS IS MONGODB SYNTAX
+});
 app.listen(8080) //every computer has thousands of ports that you can turn on and off, in this case it is localhost.
